b46292d28d7cc495790eb43e46c7371f2974635d
Njugi92 <kenkaruma.kk@gmail.com>
2023-08-15 00:51:40 +0300

    BaseModel debug

--- models/base_model.py
+++ models/base_model.py
@@ -1,76 +1,82 @@
 #!/usr/bin/python3
-"""This defines the BaseModel class."""
-import models
+"""
+base_model module :
+this module contain BaseModel class this class consider
+our base calss for our AirBnB project.
+"""
+
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
-class BaseModel:
-    """Representation of the BaseModel of the HBnB project."""
+class BaseModel():
+    """Base Model Class:
+    this class has a lot of functions and consider the base class of
+    many others classe.
+    """
     objects = []
 
     def __init__(self, *args, **kwargs):
-        """It initializes a new BaseModel.
-        Artributes:
-            *args (any): Unused.
-            **kwargs (dict): The key/value pairs of attributes.
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
+        """constructor"""
+        if kwargs:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
+
         else:
-            models.storage.new(self)
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
-    def save(self):
-        """It updates updated_at with current datetime."""
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        """This returns dictionary of the BaseModel instance.
-        Also includes the key/value pairs __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        self.__class__.objects.append(self)
 
     def __str__(self):
-        """It returns print or str representations of BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """print object representation"""
+
+        return ('[' + type(self).__name__ + '] (' +
+                str(self.id) + ') ' + str(self.__dict__))
+
+    def save(self):
+        """updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
 
     @classmethod
     def all(self):
-        """The function to print all object from specific class"""
-        print((str(model) for model in self.objects))
+        """function to print all object from specific class"""
+        print([str(model) for model in self.objects])
 
     @classmethod
     def count(self):
-        """To print the len of object in specific class"""
+        """to print the len of object in specific class"""
         print(len(self.objects))
 
     @classmethod
     def show(self, id):
-        """Function to show the object in specific class using id"""
+        """to show the object in specific class using id"""
         for object in self.objects:
             if object.id == id:
                 print(object)
                 return
-            print("** no instance found **")
+        print("** no instance found **")
 
     @classmethod
     def update(self, id, attribute_name, attribute_value):
-        """Function to update object attributes"""
+        """to update the object attributes """
+
         for value in storage.all().values():
             if value.id == id:
                 value.__dict__[attribute_name] = attribute_value
@@ -79,23 +85,23 @@ class BaseModel:
             if object.id == id:
                 object.__dict__[attribute_name] = attribute_value
                 return
-            print("** no instance found **")
+        print("** no instance found **")
 
     @classmethod
     def update_dict(self, id, attribute_dict):
-        """Function to update the object attributes from dict"""
-        for k, v in attribute_dict.items():
-            self.update(id, k, v)
+        """to update the object attributes from dict"""
+        for key, value in attribute_dict.items():
+            self.update(id, key, value)
 
     @classmethod
     def destroy(self, id):
-        """The function to remove object from class using its id"""
-        k = self.__name__ + '.' + id
-        if k in storage.all():
+        """to remove object from class using its id"""
+        key = self.__name__ + '.' + id
+        if key in storage.all():
             for i in range(len(self.objects)):
                 if self.objects[i].id == id:
                     self.objects.pop(i)
-            storage.all().pop(k)
+            storage.all().pop(key)
             storage.save()
             return
         print("** no instance found **")
